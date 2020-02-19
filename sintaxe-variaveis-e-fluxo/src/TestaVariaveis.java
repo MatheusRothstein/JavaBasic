c4bec0d3ddaf9cd14dd3faceeca25393e5df5940
Matheus <matheusrothstein1@gmail.com>
2020-02-18 21:46:01 -0300

    chapter 3 completed

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,19 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("olá novo teste");
+		
+		int idade;
+
+		idade = 30 + 7;
+		System.out.println("Sua idade é " + idade + " Parabéns");
+		idade  = 5* 7 + 2;
+		System.out.println("Sua idade é " + idade + " Parabéns");
+		idade = 30 + 10;
+		System.out.println("Sua idade é " + idade + " Parabéns");
+		
+	}
+
+}
